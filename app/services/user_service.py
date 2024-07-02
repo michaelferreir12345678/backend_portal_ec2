00419488c54f8e5f3cc151179630d9fde159a663
Michael ferreira <Michaelferreiradesousa@gmail.com>
2024-07-02 16:32:43 -0300

    Atualização

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -3,7 +3,7 @@ from app.extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 def create_user(username, password):
-    hashed_password = generate_password_hash(password, method='sha256')
+    hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
     new_user = User(username=username, password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
